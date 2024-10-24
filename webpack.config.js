cb76ad912663d17244663475d0d5b14228bd3cc4
Evren Yeniev <mail@evren.dev>
2024-10-24 10:28:35 +0200

    Add webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+import Dotenv from "dotenv-webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+
+export default (env) => {
+  const envFile = env.NODE_ENV ? `.env.${env.NODE_ENV}` : ".env.development";
+  return {
+    entry: "./src/js/index.js",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve("dist"),
+      publicPath: "/",
+      libraryTarget: "umd",
+      clean: true,
+    },
+    mode: env.NODE_ENV || "development",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+      ],
+    },
+    plugins: [
+      new Dotenv({
+        path: envFile,
+      }),
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        filename: "index.html",
+      }),
+    ],
+    watch: true,
+    devServer: {
+      static: "./dist",
+      hot: true,
+      port: 8080,
+      open: true,
+    },
+  };
+};
